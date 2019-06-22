4e0affc2a7cd935e437dae778c05981cdd608541
Paige <>
2019-06-21 21:13:30 -0600

    Timestamp now uses "as" prop, uses correct color

--- src/components/timestamp.jsx
+++ src/components/timestamp.jsx
@@ -12,32 +12,7 @@ const timeAgo = new TimeAgo("en-US");
 export const formatTime = time =>
   timeAgo.format(new Date(time * 1000), "twitter");
 
-export const TimestampTestPage = props => (
-  <div style={{ margin: "2em" }}>
-    <div />
-    <Timestamp time={1558516866} />
-    <div />
-    <Timestamp time={1558516866} to="/lmao" />
-    <div />
-    <Timestamp time={1558516866}>Text</Timestamp>
-    <div />
-    <Timestamp time={1558516866}>
-      <span>span 1</span>
-      <span>span 2</span>
-    </Timestamp>
-    <div />
-    <Timestamp time={1558516866} />
-    <div />
-    <Timestamp time={1558516866} tooltipLabel="Edited" label="*" />
-    <div />
-  </div>
-);
-
 const StyledTs = styled.span`
-  color: ${props => props.theme.container.link};
-`;
-
-const StyledTsLink = styled(Link)`
   color: ${props => props.theme.container.color};
 `;
 
@@ -64,15 +39,12 @@ export const Timestamp = ({
   const properties = {
     "data-tip": _tooltipLabel,
     "data-delay-show": 500,
+    as: to ? Link : null,
+    to: to ? to : null,
     ...props,
   };
 
-  return to ? (
-    <StyledTsLink to={to} {...properties}>
-      {children}
-      {_label}
-    </StyledTsLink>
-  ) : (
+  return (
     <StyledTs {...properties}>
       {children}
       {_label}
