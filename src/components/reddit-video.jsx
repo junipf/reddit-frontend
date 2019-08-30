5a402be7e4d0de42f50a13b31379afcc8ccec79f
Juniper <50317412+junipf@users.noreply.github.com>
2019-08-29 20:09:52 -0600

    Video big play/restart icons now colored correctly

--- src/components/reddit-video.jsx
+++ src/components/reddit-video.jsx
@@ -713,6 +713,7 @@ const Overlay = styled.div`
   left: 0;
   right: 0;
   bottom: 0;
+  color: ${({theme}) => theme.text};
   pointer-events: ${({ usePointerEvents }) =>
     usePointerEvents ? null : "none"};
   background-color: rgba(0, 0, 0, 0.5);
